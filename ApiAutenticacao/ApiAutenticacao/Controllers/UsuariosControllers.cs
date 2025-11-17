64ee8a0e7702d1c31c12080ebe1fa765bc3b6175
LUCAS ALVES CANDIDO <lucas.acandido1@senacsp.edu.br>
2025-11-17 19:57:59 -0300

    Pasta 07

---
+++ ApiAutenticacao/ApiAutenticacao/Controllers/UsuariosControllers.cs
@@ -0,0 +1,80 @@
+﻿using ApiAutenticacao.Data;
+using ApiAutenticacao.Models;
+using ApiAutenticacao.Models.DTO;
+using BCrypt.Net;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System.ComponentModel.DataAnnotations;
+using static BCrypt.Net.BCrypt;
+
+namespace ApiAutenticacao.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosControllers : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UsuariosControllers(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpPost("cadastrar")]
+        public async Task<ActionResult> CadastrarUsuario([FromBody] CadastroUsuarioDTO dadosUsuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+
+            }
+            Usuario? usuarioExistente = await _context.Usuarios.
+                FirstOrDefaultAsync(usuario => usuario.Email == dadosUsuario.Email);
+
+            if (usuarioExistente != null)
+            {
+                return BadRequest(new { mensagem = "Email já cadastrado" });
+            }
+            Usuario usuario = new Usuario
+            {
+                Nome = dadosUsuario.Nome,
+                Email = dadosUsuario.Email,
+                Senha = HashPassword(dadosUsuario.Senha),
+                ConfirmarSenha = dadosUsuario.ConfirmarSenha
+            };
+
+
+            _context.Usuarios.Add(usuario);
+            await _context.SaveChangesAsync();
+
+            return Ok(new 
+            {
+                id = usuario.Id,
+                nome = usuario.Nome,
+                email = usuario.Email
+                
+            });
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
+    
+
+
+     
---
+++ ApiAutenticacao/ApiAutenticacao/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiAutenticacao.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ApiAutenticacao.Models.Usuario> Usuarios
+        {
+            get; set;
+
+
+
+        }
+
+
+    }
+}
+       
