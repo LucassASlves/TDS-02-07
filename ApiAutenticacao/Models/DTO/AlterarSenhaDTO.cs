1a590f9b87b17143eb68af7dab4c307b6c061691
LUCAS ALVES CANDIDO <lucas.acandido1@senacsp.edu.br>
2025-12-01 22:16:31 -0300

    Inserindo método alterar senha

---
+++ ApiAutenticacao/Models/DTO/AlterarSenhaDTO.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace apiAutenticacao.Models.DTO
+{
+    public class AlterarSenhaDTO
+    {
+
+        [Required(ErrorMessage = "O email é obrigatório")]
+        [EmailAddress(ErrorMessage = "Email inválido")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "A senha é obrigatória")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "A senha deve ter entre 6 e 100 caracteres")]
+        public string SenhaAtual { get; set; } = string.Empty;
+
+
+        [Required(ErrorMessage = "A senha é obrigatória")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "A senha deve ter entre 6 e 100 caracteres")]
+        public string NovaSenha { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "A senha é obrigatória")]
+        [Compare("Senha", ErrorMessage = "As senhas não conferem")]
+        public string ConfirmarNovaSenha { get; set; } = string.Empty;
+    }
+}
---
+++ ApiAutenticacao/Services/AuthService.cs
@@ -0,0 +1,156 @@
+﻿using apiAutenticacao.Data;
+using apiAutenticacao.Models;
+using apiAutenticacao.Models.DTO;
+using apiAutenticacao.Models.Response;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using static BCrypt.Net.BCrypt;
+
+
+namespace apiAutenticacao.Services
+{
+    public class AuthService
+    {
+
+        private readonly AppDbContext _context;
+
+
+        public AuthService(AppDbContext context) {
+
+            _context = context;
+        
+        }
+
+
+        public async Task<ResponseLogin> Login(LoginDTO dadosUsuario)
+        {
+
+            Usuario? usuarioEncontrado = await _context.Usuarios.
+                FirstOrDefaultAsync(usuario => usuario.Email == dadosUsuario.Email);
+
+            if (usuarioEncontrado != null)
+            {
+                bool isValidPassword = Verify(dadosUsuario.Senha, usuarioEncontrado.Senha);
+
+                if (isValidPassword)
+                {
+                    return new ResponseLogin { 
+                    
+                        Erro = false,
+                        Message = "Login Realizado com sucesso",
+                        Usuario = new Usuario { 
+                        
+                            Nome = usuarioEncontrado.Nome,
+                            Email = usuarioEncontrado.Email
+                        
+                        }
+
+                    };
+
+                }
+
+                return new ResponseLogin { 
+                
+                    Erro = true,
+                    Message = "Login não realizado. Email ou senha incorretos",
+                    
+                };
+
+            }
+
+            return new ResponseLogin { 
+            
+                Erro = true,
+                Message = "Usuário não encontrado",
+                
+            };
+
+
+        }
+
+        public async Task<ResponseCadastro> CadastrarUsuarioAsync(CadastroUsuarioDTO dadosUsuarioCadastro) {
+
+            Usuario? usuarioExistente = await _context.Usuarios.
+               FirstOrDefaultAsync(usuario => usuario.Email == dadosUsuarioCadastro.Email);
+
+            if (usuarioExistente != null)
+            {
+
+
+                return new ResponseCadastro { 
+                
+                    Erro = true,
+                    Message = "Este email já está cadastrado no sistema"
+                
+                };
+
+                
+            }
+
+            Usuario usuario = new Usuario
+            {
+
+                Nome = dadosUsuarioCadastro.Nome,
+                Email = dadosUsuarioCadastro.Email,
+                Senha = HashPassword(dadosUsuarioCadastro.Senha),
+                ConfirmarSenha = HashPassword(dadosUsuarioCadastro.ConfirmarSenha)
+
+
+            };
+
+            _context.Usuarios.Add(usuario);
+            await _context.SaveChangesAsync();
+
+            return new ResponseCadastro { 
+            
+                Erro = false,
+                Message = "Usuário cadastrado com sucesso",
+                Usuario = usuario
+                
+
+            };
+        
+
+        }
+
+        public async Task<ResponseLogin> AlterarSenhaAsync(AlterarSenhaDTO dadosAlterarSenha) {
+            Usuario? usuarioEncontrado = await _context.Usuarios.
+                FirstOrDefaultAsync(usuario => usuario.Email == dadosAlterarSenha.Email);
+            if (usuarioEncontrado == null) {
+                return new ResponseCacheAttribute { 
+                
+                    = false,
+                Message = "Usuário cadastrado com sucesso",
+                Usuario = usuario
+                
+                
+                };
+            }
+            bool isValidPassword = Verify(dadosAlterarSenha.SenhaAtual, usuarioEncontrado.Senha);
+            if (!isValidPassword) {
+                return new ResponseCacheAttribute { 
+                
+                    Erro = true,
+                    Message = "Senha atual incorreta"
+                
+                };
+            }
+            usuarioEncontrado.Senha = HashPassword(dadosAlterarSenha.NovaSenha);
+            usuarioEncontrado.ConfirmarSenha = HashPassword(dadosAlterarSenha.ConfirmarNovaSenha);
+            _context.Usuarios.Update(usuarioEncontrado);
+            await _context.SaveChangesAsync();
+            return new ResponseCacheAttribute { 
+            
+                Erro = false,
+                Message = "Senha alterada com sucesso"
+            
+            };
+
+        }
+
+
+
+
+    }
+}
